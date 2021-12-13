3fd99a19a7f04872986f9f1a0519805d72697a7e
jacquetta <jacquettavb@hotmail.com>
2021-12-13 13:40:07 -0600

    added pseudocode for plan

---
+++ dayTwo.py
@@ -0,0 +1,14 @@
+# - open and read file
+# - create a dictionary to hold direction as key and integer as value
+# - create two variables one called horizontal and second called depth
+# - both variables will have a starting value at 0
+# - will need to create a loop 
+# - loop through dictionary keys 
+# - create if/elif/else statements to check key 
+# - if key is equal to 'forward' add value to variable horizontal
+# - if key is not equal to 'forward' check to see if key is equal to 'down'
+# - if key is equal to 'down' add value to variable depth
+# - if key is not equal to 'down' check to see if key is equal to 'up'
+# - if key is equal to 'up' subtract value to variable depth
+# - continue until loop comes to the end
+# - at the end print out horizontal variable multiplied by depth variable
