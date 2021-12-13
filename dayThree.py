242eac7f61659fbbdcdcbf040829c61fb7fe8bc1
jacquetta <jacquettavb@hotmail.com>
2021-12-13 15:58:17 -0600

    added pseudocode

---
+++ dayThree.py
@@ -0,0 +1,22 @@
+# find the power consumption by multiplying gamma rate and epsilon rate
+# find the gamma rate from binary input
+# find the epsilon rate from binary input
+# create three variables called power_consumption, gamma_rate, epsilon_rate
+# create variables for countZero and countOne and add + 1 for each number
+# create variables for each bit number position
+# create a list from binary.txt file
+# loop through list
+# loop through current item
+# check each bit number in each position 
+# check if first bit is one or zero 
+# if bit is zero add plus one to zero variable
+# if bit is one add plus one to one variable
+# repeat until last bit
+# check if first bit has zero variable is greater than one variable if True first bit in gamma rate is zero add to  gamma rate variable
+# then add one variable to epsilon_rate variable
+# else if first bit  one variable is greater than zero variable  than first bit in gamma rate is one add to gamma rate variable
+# then add zero variable to epsilon_rate variable
+# continue checking until last bit
+# convert gamma rate binary into decimal variable
+# convert epsilon rate binary into decimal variable
+# to get power consumption print gamma_rate * epsilon_rate
